5082bbaae68212e72b19254069d92084c0f24069
Holylogus <hamar.patrik94@gmail.com>
2024-01-30 12:18:32 +0100

    WelcomeSection start

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/WelcomeSection.css'
 
 function WelcomeSection() {
   return (
-    <div>
-      
+    <div className='Welcome'>
+      <h1><span>Willkommen bei MS Dienstleistung</span></h1>
     </div>
   )
 }
