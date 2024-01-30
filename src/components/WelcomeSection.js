f90b2461bf2adfe4d27fc604c016e87eabab192d
Holylogus <hamar.patrik94@gmail.com>
2024-01-30 13:32:52 +0100

    Welcome

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import '../styles/WelcomeSection.css'
+import React from "react";
+import "../styles/WelcomeSection.css";
+import Carousel from 'react-bootstrap/Carousel';
+import firstImage from '../images/backGround.jpg'
+import Image from 'react-bootstrap/Image';
 
 function WelcomeSection() {
   return (
-    <div className='Welcome'>
-      <h1><span>Willkommen bei MS Dienstleistung</span></h1>
-    </div>
-  )
+    <Carousel>
+      <Carousel.Item>
+        <Image src={firstImage} fluid/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <Image src={firstImage} fluid/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <Image src={firstImage} fluid/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default WelcomeSection
+export default WelcomeSection;
